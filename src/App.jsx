d32b6701389185d0ce28ed491eafd06814be8bff
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-02-25 21:06:06 +0300

    add react routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Navbar />
+          <Route path="/" element={<Shop />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
