a88e1dc5794f34c33439b3a04fe629ede3c5377c
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-02-25 21:20:33 +0300

    creates getTotalCartAmount

---
+++ src/context/shop-context.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+import { PRODUCTS } from "../products";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; i < PRODUCTS.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
+
+export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
+    }
+    return totalAmount;
+  };
+};
