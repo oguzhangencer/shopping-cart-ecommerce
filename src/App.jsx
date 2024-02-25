0fd7f33da0079c84290c8f8c62a1e7408a7fca4c
Oguzhan Gok <dev.oguzhangencer@gmail.com>
2024-02-26 01:23:11 +0300

    changes importes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ShopContextProvider } from "./context/shop-context";
 import { Navbar } from "./components/navbar";
-import { Shop } from "./pages/shop";
+import { Shop } from "./pages/shop/shop";
 import { Contact } from "./pages/contact";
-import { Cart } from "./pages/cart";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
